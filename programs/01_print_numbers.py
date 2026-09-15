78e49042c34398f525c3e4da7e386d5c01202b03
abhinav <abhinavpadige06@gmail.com>
2026-09-15 17:42:19 +0530

    Add Program 1: Simple for-loop to print numbers 1 to 10

---
+++ programs/01_print_numbers.py
@@ -0,0 +1,12 @@
+"""
+Program 1: Simple for-loop to print numbers 1 to 10
+Concept: Loops (for-loop)
+"""
+
+def print_numbers():
+    """Print numbers from 1 to 10 using a for-loop."""
+    for i in range(1, 11):
+        print(i)
+
+if __name__ == "__main__":
+    print_numbers()
